82a7a9f779ebdfb2400a3ccb2e4eedc5ddd3fd91
krishn-kanta-das <krishnakanta.it.aec@gmail.com>
2025-03-12 13:40:42 +0530

    update

---
+++ ReplaceThirdCharacter.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+
+public class ReplaceThirdCharacter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+        
+        StringBuilder modifiedString = new StringBuilder(input);
+        
+        for (int i = 2; i < modifiedString.length(); i += 3)
+         {
+            
+            modifiedString.setCharAt(i, '*');
+        }
+        
+        System.out.println("Modified string: " + modifiedString.toString());
+    }
+}
