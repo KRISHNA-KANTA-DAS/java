4c7257aa5cb30f3071d778c22ab4674db6c5f17f
krishn-kanta-das <krishnakanta.it.aec@gmail.com>
2025-03-20 19:14:05 +0530

    newupdate

--- decimaltobinary.java
+++ decimaltobinary.java
@@ -5,14 +5,38 @@ public class decimaltobinary {
     Scanner sc= new Scanner(System.in);
     int a;
     a=sc.nextInt();
-     StringBuilder sc1=new StringBuilder();
-    while(a>0){
-     
-     sc1.append(a%2);
-     a=a/2;
+    System.out.println("Choose your approach");
+    int choice= sc.nextInt();
+    switch(choice){
+        case 1:
+        {
+            StringBuilder sc1=new StringBuilder();
+            while(a>0){
+             
+             sc1.append(a%2);
+             a=a/2;
+        
+            }
+            System.out.println(sc1.reverse().toString());
+        break;
+        }
+        case 2:
+        {
+            int power=0,binary=0,remainder=0;
 
+            while(a>0){
+               remainder =a%2;
+               binary+=remainder*(int)Math.pow(10,power);
+               power++;
+                 a=a/2;  
+
+
+              }
+              System.out.println(binary);
+
+ break;
+        }
     }
-    System.out.println(sc1.reverse().toString());
-
+     
     }
 }
